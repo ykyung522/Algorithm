e2fbfa15b55ec0731c9b63269799b2e319803fcb
L-LIFE <141551829+L-LIFE@users.noreply.github.com>
2024-09-30 23:26:06 +0900

    Update a와 b 출력하기.c

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.c
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.c
@@ -6,4 +6,8 @@ int main(void) {
     scanf("%d %d", &a, &b);
     printf("a = %d\nb = %d\n", a, b);
     return 0;
+    //n;main(i){for(;~i;printf("%c = %d\n",98-i--,n))scanf("%d",&n);}
+    //다른 형태의 풀이 과정
+    //비트 NOT 연산자를 사용하여 98-i로 아스키 코드 값으로 변환됨. a부터 j까지 출력을 생성
+    
 }
