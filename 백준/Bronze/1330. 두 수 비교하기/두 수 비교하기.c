b9baee992bf73ea64e6e6071b9e6e65c7c078c38
L-LIFE <141551829+L-LIFE@users.noreply.github.com>
2025-01-27 21:31:21 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int A,B;
+    scanf("%d %d", &A,&B);
+    if(A>B){
+        printf(">\n");
+    }else if(A<B){
+        printf("<\n");
+    }else{
+        printf("==\n");
+    }
+    return 0;
+}
