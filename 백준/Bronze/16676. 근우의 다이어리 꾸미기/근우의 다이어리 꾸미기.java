96fbe5eef3f5e8f4c554e2ea0cce9276160b1e4e
L-LIFE <141551829+L-LIFE@users.noreply.github.com>
2025-01-31 22:13:32 +0900

    [Bronze II] Title: 근우의 다이어리 꾸미기, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/16676. 근우의 다이어리 꾸미기/근우의 다이어리 꾸미기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    if (n >= 0 && n <= 10) {
+      System.out.println(1);
+    } else {
+      int k = 2;
+      for (int i=11; true; i=i*10+1) {
+        if (n >= i && n <= i * 10) {
+          System.out.println(k);
+          break;
+        } k++;
+      }
+    }
+  }
+}
